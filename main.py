338147809d156d98b8acb544e3ff1522f946775a
baixiaobest <baixiaohuang1@gmail.com>
2014-11-08 20:02:38 -0800

    set up communication
    set up communication between Bluetooth port and arduino usb port

---
+++ main.py
@@ -0,0 +1,21 @@
+import serial
+import getopt, sys
+import select
+
+mySerial = serial.Serial('/dev/ttyACM0', 9600)
+
+def getSerial():
+    return mySerial.readline()
+
+def checkStdin(callback):
+    while sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
+        data = sys.stdin.readline()
+        callback(data)
+
+def printOut(data):
+    mySerial.write(data)
+
+
+if __name__=="__main__":
+    while True:
+        checkStdin(printOut)
